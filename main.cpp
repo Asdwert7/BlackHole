476b7dfa52bd95426ee3f9c7dfb4b72ad5563942
asdwert7 <asdwert309@gmail.com>
2025-10-24 01:18:22 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <GL/glew.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#ifndef M_PI
+#endif
+using namespace glm;
+using Clock = std::chrono::high_resolution_clock;
+
+
+struct Engine
+{
+    GLHWwindow* window;
+}
+
+
+
+int main()
+{
+
+
+    return 0;
+}
+
